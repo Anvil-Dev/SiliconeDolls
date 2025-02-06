c5321fbaff76d5f8d0232ddada5609b3bdc5d1a9
moelobster <2211479860@qq.com>
2025-02-06 16:44:56 +0800

    增加look和turn

--- src/main/java/dev/anvilcraft/rg/sd/command/PlayerCommand.java
+++ src/main/java/dev/anvilcraft/rg/sd/command/PlayerCommand.java
@@ -1,6 +1,7 @@
 package dev.anvilcraft.rg.sd.command;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.FloatArgumentType;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
@@ -24,6 +25,7 @@ import net.minecraft.commands.arguments.GameModeArgument;
 import net.minecraft.commands.arguments.coordinates.Coordinates;
 import net.minecraft.commands.arguments.coordinates.RotationArgument;
 import net.minecraft.commands.arguments.coordinates.Vec3Argument;
+import net.minecraft.core.Direction;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
@@ -99,29 +101,60 @@ public class PlayerCommand {
                         )
                         .then(
                             Commands.literal("sneak")
-                                .executes(ctx-> sneak(ctx,true))
+                                .executes(ctx -> sneak(ctx, true))
                         )
                         .then(
                             Commands.literal("unsneak")
-                                .executes(ctx-> sneak(ctx,false))
+                                .executes(ctx -> sneak(ctx, false))
                         )
                         .then(
                             Commands.literal("sprint")
-                                .executes(ctx-> sprint(ctx,true))
+                                .executes(ctx -> sprint(ctx, true))
                         )
                         .then(
                             Commands.literal("unsprint")
-                                .executes(ctx-> sprint(ctx,false))
+                                .executes(ctx -> sprint(ctx, false))
+                        )
+                        .then(
+                            Commands.literal("mount")
+                                .executes(ctx -> mount(ctx, true))
+                        )
+                        .then(
+                            Commands.literal("dismount")
+                                .executes(ctx -> mount(ctx, false))
+                        ).then(
+                            Commands.literal("look")
+                                .then(
+                                    Commands.literal("at")
+                                        .then(
+                                            Commands.argument("pos", Vec3Argument.vec3())
+                                                .executes(PlayerCommand::lookAt)
+                                        )
+                                )
+                                .then(
+                                    Commands.argument("direction", StringArgumentType.word())
+                                        .suggests(PlayerCommand::suggestDirection)
+                                        .executes(PlayerCommand::lookDirection)
+                                )
+                        ).then(
+                            Commands.literal("turn")
+                                .then(
+                                    Commands.argument("x", FloatArgumentType.floatArg())
+                                        .then(
+                                            Commands.argument("y", FloatArgumentType.floatArg())
+                                                .executes(PlayerCommand::turn)
+                                        )
+                                )
+                                .then(
+                                    Commands.argument("rotation", StringArgumentType.word())
+                                        .suggests(PlayerCommand::suggestRotation)
+                                        .executes(PlayerCommand::turnRotation)
+                                )
                         )
                         .then(
                             Commands.literal("swapHands")
                         )
-                        .then(
-                            Commands.literal("look")
-                        )
-                        .then(
-                            Commands.literal("turn")
-                        )
+
                         .then(
                             Commands.literal("move")
                         )
@@ -137,12 +170,6 @@ public class PlayerCommand {
                         .then(
                             Commands.literal("shadow")
                                 .executes(PlayerCommand::shadowPlayer)
-                        )
-                        .then(
-                            Commands.literal("mount")
-                        )
-                        .then(
-                            Commands.literal("dismount")
                         )
                         .then(
                             Commands.literal("stop")
@@ -163,6 +190,14 @@ public class PlayerCommand {
 
     private static @NotNull CompletableFuture<Suggestions> suggestAction(final CommandContext<CommandSourceStack> context, final SuggestionsBuilder builder) {
         return SharedSuggestionProvider.suggest(new String[]{"jump", "use", "attack"}, builder);
+    }
+
+    private static @NotNull CompletableFuture<Suggestions> suggestDirection(final CommandContext<CommandSourceStack> context, final SuggestionsBuilder builder) {
+        return SharedSuggestionProvider.suggest(new String[]{"west", "east", "north", "south", "up", "down"}, builder);
+    }
+
+    private static @NotNull CompletableFuture<Suggestions> suggestRotation(final CommandContext<CommandSourceStack> context, final SuggestionsBuilder builder) {
+        return SharedSuggestionProvider.suggest(new String[]{"left", "forward", "right", "back"}, builder);
     }
 
     public static int spawnPlayer(@NotNull CommandContext<CommandSourceStack> context) {
@@ -262,7 +297,7 @@ public class PlayerCommand {
         }
     }
 
-    public static int sneak(@NotNull CommandContext<CommandSourceStack> context,boolean doSneak) {
+    public static int sneak(@NotNull CommandContext<CommandSourceStack> context, boolean doSneak) {
         FakePlayer player = isFakePlayerValid(context);
         if (player == null) return 0;
         PlayerActionPack actionPack = ((ServerPlayerInjector) player).getActionPack();
@@ -270,7 +305,7 @@ public class PlayerCommand {
         return 1;
     }
 
-    public static int sprint(@NotNull CommandContext<CommandSourceStack> context,boolean doSprint) {
+    public static int sprint(@NotNull CommandContext<CommandSourceStack> context, boolean doSprint) {
         FakePlayer player = isFakePlayerValid(context);
         if (player == null) return 0;
         PlayerActionPack actionPack = ((ServerPlayerInjector) player).getActionPack();
@@ -278,10 +313,79 @@ public class PlayerCommand {
         return 1;
     }
 
+    public static int mount(@NotNull CommandContext<CommandSourceStack> context, boolean doMount) {
+        FakePlayer player = isFakePlayerValid(context);
+        if (player == null) return 0;
+        PlayerActionPack actionPack = ((ServerPlayerInjector) player).getActionPack();
+        actionPack.mount(doMount);
+        return 1;
+    }
 
+    public static int lookDirection(@NotNull CommandContext<CommandSourceStack> context) {
+        String direction = ModCommands.getArg(context, "direction", StringArgumentType::getString);
+        if (direction == null) {
+            return 0;
+        }
+        Direction d = getDirection(direction);
+        if (d == null) {
+            context.getSource().sendFailure(TranslationUtil.trans("silicone_dolls.commands.tips.invalid_direction").withStyle(ChatFormatting.RED));
+            return 0;
+        }
+        FakePlayer player = isFakePlayerValid(context);
+        if (player == null) return 0;
+        PlayerActionPack actionPack = ((ServerPlayerInjector) player).getActionPack();
+        actionPack.look(d);
+        return 1;
+    }
 
+    public static int lookAt(@NotNull CommandContext<CommandSourceStack> context) {
+        FakePlayer player = isFakePlayerValid(context);
+        if (player == null) return 0;
+        PlayerActionPack actionPack = ((ServerPlayerInjector) player).getActionPack();
+        Vec3 vec3 = ModCommands.getArg(context, "pos", Vec3Argument::getVec3);
+        if (vec3 == null) {
+            context.getSource().sendFailure(TranslationUtil.trans("silicone_dolls.commands.tips.invalid_position").withStyle(ChatFormatting.RED));
+            return 0;
+        }
+        actionPack.lookAt(vec3);
+        return 1;
+    }
 
-    public static int stopActions(@NotNull CommandContext<CommandSourceStack> context){
+    public static int turnRotation(@NotNull CommandContext<CommandSourceStack> context){
+        String rotation = ModCommands.getArg(context, "rotation", StringArgumentType::getString);
+        if (rotation == null) {
+            return 0;
+        }
+        int r = getAngle(rotation);
+        if(r == -1){
+            context.getSource().sendFailure(TranslationUtil.trans("silicone_dolls.commands.tips.invalid_rotation").withStyle(ChatFormatting.RED));
+            return 0;
+        }
+        FakePlayer player = isFakePlayerValid(context);
+        if (player == null) return 0;
+        PlayerActionPack actionPack = ((ServerPlayerInjector) player).getActionPack();
+        actionPack.turn(r,0);
+        return 1;
+    }
+
+    public static int turn(@NotNull CommandContext<CommandSourceStack> context){
+        FakePlayer player = isFakePlayerValid(context);
+        if (player == null) return 0;
+        PlayerActionPack actionPack = ((ServerPlayerInjector) player).getActionPack();
+        float x;
+        float y;
+        try{
+            x= FloatArgumentType.getFloat(context, "x");
+            y= FloatArgumentType.getFloat(context, "y");
+        }catch (IllegalArgumentException ignored){
+            context.getSource().sendFailure(TranslationUtil.trans("silicone_dolls.commands.tips.invalid_rotation").withStyle(ChatFormatting.RED));
+            return 0;
+        }
+        actionPack.turn(x,y);
+        return 1;
+    }
+
+    public static int stopActions(@NotNull CommandContext<CommandSourceStack> context) {
         FakePlayer player = isFakePlayerValid(context);
         if (player == null) return 0;
         PlayerActionPack actionPack = ((ServerPlayerInjector) player).getActionPack();
@@ -320,6 +424,28 @@ public class PlayerCommand {
             case "drop_item" -> PlayerActionPack.ActionType.DROP_ITEM;
             case "drop_stack" -> PlayerActionPack.ActionType.DROP_STACK;
             case null, default -> null;
+        };
+    }
+
+    public static Direction getDirection(String direction) {
+        return switch (direction) {
+            case "west" -> Direction.WEST;
+            case "east" -> Direction.EAST;
+            case "north" -> Direction.NORTH;
+            case "south" -> Direction.SOUTH;
+            case "up" -> Direction.UP;
+            case "down" -> Direction.DOWN;
+            case null, default -> null;
+        };
+    }
+
+    public static int getAngle(String rotation) {
+        return switch (rotation) {
+            case "forward" -> 0;
+            case "right" -> 90;
+            case "back" -> 180;
+            case "left" -> 270;
+            case null, default -> -1;
         };
     }
 }
