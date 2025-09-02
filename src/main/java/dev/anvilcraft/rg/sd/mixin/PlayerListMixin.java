274d6f67ccf4dc9d7463541b9d96529df74a5dbb
Gugle <administration@ztxy666.cn>
2025-09-02 18:34:18 +0800

    build(dependencies): 更新 rolling_gate 版本并优化代码
    - 将 rolling_gate 版本从1.1.0+build.60 升级到 1.1.0+build.62
    - 更新 PlayerListMixin 中的 fixStartingPos 方法参数
    - 修改 ServerCommonPacketListenerImplMixin 中的 send 方法参数

--- src/main/java/dev/anvilcraft/rg/sd/mixin/PlayerListMixin.java
+++ src/main/java/dev/anvilcraft/rg/sd/mixin/PlayerListMixin.java
@@ -4,24 +4,27 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import dev.anvilcraft.rg.sd.entity.FakePlayer;
 import dev.anvilcraft.rg.sd.entity.FakePlayerNetHandler;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.Connection;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.CommonListenerCookie;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import net.minecraft.server.players.PlayerList;
+import net.minecraft.util.ProblemReporter;
+import net.minecraft.world.level.storage.ValueInput;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Optional;
+
 @Mixin(PlayerList.class)
 abstract class PlayerListMixin {
     @Inject(method = "load", at = @At(value = "RETURN"))
-    private void fixStartingPos(ServerPlayer serverPlayerEntity_1, CallbackInfoReturnable<CompoundTag> cir) {
-        if (serverPlayerEntity_1 instanceof FakePlayer player) {
+    private void fixStartingPos(ServerPlayer serverPlayer, ProblemReporter problemReporter, CallbackInfoReturnable<Optional<ValueInput>> cir) {
+        if (serverPlayer instanceof FakePlayer player) {
             player.fixStartingPosition.run();
         }
     }
--- src/main/java/dev/anvilcraft/rg/sd/mixin/combat/ServerCommonPacketListenerImplMixin.java
+++ src/main/java/dev/anvilcraft/rg/sd/mixin/combat/ServerCommonPacketListenerImplMixin.java
@@ -2,6 +2,7 @@ package dev.anvilcraft.rg.sd.mixin.combat;
 
 import com.mojang.authlib.GameProfile;
 import dev.anvilcraft.rg.sd.entity.FakePlayer;
+import io.netty.channel.ChannelFutureListener;
 import net.minecraft.network.PacketSendListener;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.server.MinecraftServer;
@@ -24,10 +25,10 @@ public abstract class ServerCommonPacketListenerImplMixin {
     public abstract GameProfile getOwner();
 
     @Inject(
-        method = "send(Lnet/minecraft/network/protocol/Packet;Lnet/minecraft/network/PacketSendListener;)V",
+        method = "send(Lnet/minecraft/network/protocol/Packet;Lio/netty/channel/ChannelFutureListener;)V",
         at = @At("HEAD"), cancellable = true
     )
-    private void send(Packet<?> packet, PacketSendListener listener, CallbackInfo ci) {
+    private void send(Packet<?> packet, ChannelFutureListener sendListener, CallbackInfo ci) {
         ServerPlayer player = this.server.getPlayerList().getPlayer(this.getOwner().getId());
         if (player instanceof FakePlayer) ci.cancel();
     }
